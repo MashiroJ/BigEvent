b25d91178a65fe07843749424022b98b15732e77
Mashiro <1146446176@qq.com>
2024-10-22 15:13:32 +0800

    feat: 初始化 BigEvent 项目- 添加 Spring Boot 项目结构和基本配置 - 配置 MySQL 数据源 - 添加 MyBatis 依赖 - 创建基本的单元测试 - 设置项目编码为 UTF-8 - 添加 Apifox Uploader 配置 - 创建 IntelliJ IDEA 项目配置文件 - 添加 Maven 项目对象模型文件 (pom.xml)

---
+++ src/main/java/itheima/BigEventApplication.java
@@ -0,0 +1,32 @@
+package itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class BigEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventApplication.class, args);
+        System.out.println("                           ┏━┓     ┏━┓\n" +
+                "                          ┏┛ ┻━━━━━┛ ┻┓\n" +
+                "                          ┃　　　　　　 ┃\n" +
+                "                          ┃　　　━　　　┃\n" +
+                "                          ┃　┳┛　  ┗┳　┃\n" +
+                "                          ┃　　　　　　 ┃\n" +
+                "                          ┃　　　┻　　　┃\n" +
+                "                          ┃　　　　　　 ┃\n" +
+                "                          ┗━┓　　　┏━━━┛\n" +
+                "                            ┃　　　┃   神兽保佑\n" +
+                "                            ┃　　　┃   代码无BUG！\n" +
+                "                            ┃　　　┗━━━━━━━━━┓\n" +
+                "                            ┃   BitEvent    ┣┓\n" +
+                "                            ┃      启动      ┏┛\n" +
+                "                            ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛\n" +
+                "                              ┃ ┫ ┫   ┃ ┫ ┫\n" +
+                "                              ┗━┻━┛   ┗━┻━┛\n" +
+                "           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    }
+}
